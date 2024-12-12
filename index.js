725ccd23b46c60ab8b271132627aab1f05153eda
Psalmcarter <topeadeoye8@gmail.com>
2024-12-12 20:01:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Coding challenge 1
+function multiplyNumbers(a, b) {
+    return a * b;
+  }
+  console.log(multiplyNumbers(2, 5 - 2));
+  console.log(multiplyNumbers(2 * 2, 10 / 2));
+  
+  // Coding challenge 2
+  const number = 7;
+  if (number / 2) {
+    console.log(`${number} is odd`);
+  } else {
+    console.log(`${number} is even`);
+  }
+  //I used template literal because I forgot how to add "is odd/even" at the back of the variable (number)
+  
+  //chanllenge 3
+  const isMorning = true;
+  let salute = isMorning ? "Good Morning" : "Good Evening";
+  console.log(salute);
+  
+  //Challenge 4
+  function canEnterClub(age, hasInvitation) {
+    return age >= 18 || hasInvitation;
+  }
+  console.log(canEnterClub(18, true));
+  console.log(canEnterClub(16, false));
+  
+  //Challenge 5
+  function simpleCalculator(num1, num2, operator) {}
+  //I don't understand the question
+  
